43653f76c81e41f9482624b5d9545f85f90f0e6a
felipesaadi <felipesaadisiegert@gmail.com>
2024-11-06 22:06:40 -0300

    feat: add socket

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
 const express = require("express")
 const http = require("http")
 const path = require("path")
+const socketIO = require('socket.io')
 
 const app = express()
 const server = http.createServer(app)
+const io = socketIO(server)
 
 server.listen(3000, () => {
   console.log("Servidor rodando em http://localhost:3000")
 })
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+io.on('connection', (socket) => {
+  console.log("Conexão detecada...")
+})
