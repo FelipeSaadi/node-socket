1dd071388913a4f46b1e855caa26544b01097be2
felipesaadi <felipesaadisiegert@gmail.com>
2024-11-06 22:39:19 -0300

    feat: add login logic

--- src/public/main.js
+++ src/public/main.js
@@ -1,1 +1,26 @@
 const socket = io()
+let username = ''
+let userList = []
+
+let loginPage = document.querySelector("#login-page")
+let chatPage = document.querySelector("#chat-page")
+
+let nameInput = document.querySelector("#name-input")
+let chatInput = document.querySelector("#chat-input")
+
+loginPage.style.display = 'flex'
+chatPage.style.display = 'none'
+
+nameInput.addEventListener('keyup', (e) => {
+  if(e.keyCode === 13) {
+    let name = nameInput.value.trim()
+
+    if(name != '') {
+      username = name
+      document.title = 'Chat ('+ username + ')'
+
+      loginPage.style.display = 'none'
+      chatPage.style.display = 'flex'
+    }
+  }
+})
